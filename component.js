66dc11159dba39db4a96f5480bc642703b0c3c0f
oscar <ozotto@hotmail.com>
2019-03-29 13:55:24 +0100

    add test

---
+++ component.js
@@ -0,0 +1,8 @@
+var datum = dataNeo;
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue!',
+    items: datum
+  }
+})
---
+++ data.js
@@ -0,0 +1,9 @@
+var dataNeo = [
+	{nom : "Arena 1 TEST", alt:"Neo xl arena1 Palexpo", dimension: "L x H: 29 x 9m", tafif: "CHF 60'000", cout: "CHF 9'900", image:"https://via.placeholder.com/150" },
+	{nom : "Arena 2", alt:"Neo xl arena2 Palexpo", dimension: "L x H: 18.4 x 7.8m", tafif: "CHF 36'000", cout: "CHF 6'300", image:"https://via.placeholder.com/150" },
+	{nom : "Arena 1", alt:"Neo xl arena2 Palexpo", dimension: "L x H: 18.4 x 7.8m", tafif: "CHF 60'000", cout: "CHF 9'900", image:"https://via.placeholder.com/150" },
+	{nom : "Arena 2", alt:"Neo xl arena2 Palexpo", dimension: "L x H: 18.4 x 7.8m", tafif: "CHF 36'000", cout: "CHF 6'300", image:"https://via.placeholder.com/150" },
+	{nom : "Halle 7.1", alt:"Neo xl arena2 Palexpo", dimension: "L x H: 24.2 x 5.7m", tafif: "CHF 16'000", cout: "CHF 4'100", image:"https://via.placeholder.com/150" },
+	{nom : "Halle 5", alt:"Neo xl arena2 Palexpo", dimension: "L x H: 18.7 x 8.7m", tafif: "CHF 22'500", cout: "CHF 6'500", image:"https://via.placeholder.com/150" }
+]
+
